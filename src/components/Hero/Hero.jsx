7ba228b74ee123b223c3265aefe947a7e613af4d
nikhilgoduguchintha <61309196+nikhilgoduguchintha@users.noreply.github.com>
2025-08-17 15:50:38 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -60,7 +60,7 @@ function Hero() {
           <a href="#contact" className={styles.secondaryButton}>
             Get In Touch
           </a>
-          <a href="../assets/resume.pdf" download="Nikhil-Resume.pdf" className={styles.secondaryButton}>
+          <a href="../../assets/resume.pdf" download="Nikhil-Resume.pdf" className={styles.secondaryButton}>
             Download resume
           </a>
         </motion.div>
